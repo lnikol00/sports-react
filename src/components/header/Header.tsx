c561f377d98106ea85be90e31c7d3e68430ec654
Koce1 <nikolac007@gmail.com>
2023-04-23 23:41:48 +0200

    editing navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import * as FcIcons from "react-icons/fc"
 import * as FiIcons from "react-icons/fi"
+import * as HiIcons from "react-icons/hi"
 import { useState } from "react"
 
 type Props = {
@@ -50,9 +51,12 @@ const Left = styled.div`
 `
 
 const Navbar = styled.nav<Props>`
+    display:flex;
+    align-items:center;
+    flex-direction:column;
     position: absolute;
     top:0;
-    left:${(props) => (props.openNavbar ? "0" : "-50%")};
+    left:${(props) => (props.openNavbar ? "0" : "-100%")};
     width:325px;
     height:100vh;
     padding-top:30px;
@@ -64,11 +68,13 @@ const Close = styled.div`
     display:flex;
     justify-content: center;
     align-items: center;
+    flex-direction:column;
     gap: 8px;
     height: 2.5em;
-    width: 3em;
+    width: 1.5em;
     padding-bottom:15px;
-    margin-right:20px;
+    /* margin-right:20px; */
+    margin-left:3em;
     cursor: pointer;
 
     div{
@@ -79,16 +85,16 @@ const Close = styled.div`
     }
 
     div:nth-child(1){
-        transform:rotate(45deg) translate(1.8em, -1.5em);
+        transform:rotate(45deg) translate(0.1em, 0.6em);
     }
     div:nth-child(2){
-        transform:rotate(-45deg) translate(0.5em, 0.8em);
+        transform:rotate(-45deg) translate(-0.3em, -0.2em);
     }
 `
 
 const Input = styled.h2`
     font-size:18px;
-    padding-right: 2em;
+    padding-right: 1em;
 `
 
 const Search = styled.div`
@@ -98,11 +104,41 @@ const Search = styled.div`
     height:40px;
     font-size: 25px;
     width:280px;
-    margin-left: 20px;
     padding-left: 10px;
+    margin-bottom:0.5em;
     padding-bottom:15px;
     border-bottom:1px solid black;
     cursor: pointer;
+`
+const MenuItems = styled.ul`
+    width:300px;
+`
+
+const MenuLink = styled.li`
+    display:flex;
+    justify-content: ${(props) => (props.content)};
+    align-items:center;
+    padding:15px 20px;
+    width:300px;
+    list-style:none;
+    margin-bottom:5px;
+
+    div{
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        cursor: pointer;
+    }
+
+    a{
+        font-size:1em;
+        font-weight:600;
+        cursor: pointer;
+    }
+
+    &:hover{
+        background-color: #ffb81b;
+    }
 `
 
 function Header() {
@@ -139,6 +175,31 @@ function Header() {
                         <div></div>
                     </Close>
                 </Search>
+                <MenuItems>
+                    <MenuLink><a>News</a></MenuLink>
+                    <MenuLink content="space-between">
+                        <a>Foootball</a>
+                        <div>
+                            <HiIcons.HiOutlineChevronRight />
+                        </div>
+                    </MenuLink>
+                    <MenuLink><a>Basketball</a></MenuLink>
+                    <MenuLink><a>Tennis</a></MenuLink>
+                    <MenuLink content="space-between">
+                        <a>Racing</a>
+                        <div>
+                            <HiIcons.HiOutlineChevronRight />
+                        </div>
+                    </MenuLink>
+                    <MenuLink><a>Handball</a></MenuLink>
+                    <MenuLink content="space-between">
+                        <a>E-sports</a>
+                        <div>
+                            <HiIcons.HiOutlineChevronRight />
+                        </div>
+                    </MenuLink>
+                    <MenuLink><a>Forum</a></MenuLink>
+                </MenuItems>
             </Navbar>
 
         </HeaderContainer>
