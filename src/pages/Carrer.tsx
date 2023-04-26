4f4b8a5b0ab95b2a76e8e10a8d8ad92b42935347
Koce1 <nikolac007@gmail.com>
2023-04-26 23:42:03 +0200

    editing carrer page

--- src/pages/Carrer.tsx
+++ src/pages/Carrer.tsx
@@ -3,16 +3,19 @@ import styled from "styled-components"
 import * as HiIcons from "react-icons/hi"
 
 const MainContainer = styled.div`
-    margin-bottom: 2000px;
+    margin-bottom: 700px;
 `
 
 const Form = styled.form`
+    display:flex;
+    justify-content:center;
+    flex-direction: column;
     position: absolute;
     top:50%;
     left: 50%;
     transform:translate(-50%, -50%);
     width: 80%;
-    height: 500px;
+    height: 550px;
     border: 1px solid black;
     border-radius: 40px;
     padding: 5em 7em;
@@ -47,7 +50,27 @@ const Top = styled.div`
 `
 
 const Center = styled.div`
-    
+    display:flex;
+    flex-direction: column;
+
+    button{
+        width: 250px;
+        height:50px;
+        outline: none;
+        background-color: inherit;
+        border: 1px solid black;
+        border-radius: 16px;
+        cursor: pointer;
+
+        &:hover{
+            background-color: rgba(255,255,255,0.2);
+        }
+    }
+
+    span{
+        padding-top: 10px;
+        font-size: 14px;
+    }
 `
 const Bottom = styled.div`
     display: flex;
@@ -57,7 +80,7 @@ const Bottom = styled.div`
     gap: 20px;
     padding: 2em 0;
 
-    input{
+    textarea{
         width: 100%;
         height:40px;
         outline: none;
@@ -66,12 +89,24 @@ const Bottom = styled.div`
         background-color:inherit;  
         border-bottom: 1px solid black;
         font-size: 18px;
+        overflow: hidden;
 
         &::placeholder{
             color: black;
         }    
     }
 
+    select{
+        width: 100%;
+        height:40px;
+        outline: none;
+        border: none;
+        padding-left: 20px;
+        background-color:inherit;  
+        border-bottom: 1px solid black;
+        font-size: 18px;
+        cursor: pointer;
+    }
     
 `
 
@@ -81,7 +116,8 @@ const Button = styled.button`
     align-items: center;
     gap: 10px;
     height: 60px;
-    padding: 12px 20px;
+    width:125px;
+    padding: 16px 20px;
     border-radius: 30px;
     outline: none;
     border: none;
@@ -118,14 +154,20 @@ function Carrer() {
                     />
                 </Top>
                 <Center>
-                    Ostavi posli za CV
+                    <button>Upload CV/Resume</button>
+                    <span>Minimum file size 5MB</span>
                 </Center>
                 <Bottom>
-                    <input
-                        type="text"
+                    <textarea
                         placeholder="Tell us something about yourself"
                     />
-                    Dodaj bolji select
+                    <select>
+                        <option>Facebook</option>
+                        <option>Instagram</option>
+                        <option>LinkedIn</option>
+                        <option>Word of mouth</option>
+                        <option>Other</option>
+                    </select>
                 </Bottom>
                 <Button>
                     <HiIcons.HiOutlineChevronRight />
